d728dd07ebf77b6912998406807c48deb64d089b
Gal Malka <56474649+galmalka21@users.noreply.github.com>
2025-01-05 21:45:29 +0200

    cors update

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const http = require('http');
+const https = require('https');
 const app = express();
-const path = require('path')
-const sql = require("./mssql");
 const generalRoute = require('./routes/general')
 const cookieParser = require('cookie-parser');
 const websocket = require('./middleware/websocket');
 const port = 3000;
 
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://witty-rock-00b5d0803.4.azurestaticapps.net/'], // Frontend URL
-    credentials: true, // Allow cookies and credentials
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allow necessary HTTP methods
-    allowedHeaders: 'Content-Type,Authorization,ngrok-skip-browser-warning', // Allow the 'ngrok-skip-browser-warning' header
-}));
+const corsOptions = {
+    origin: 'https://witty-rock-00b5d0803.4.azurestaticapps.net', // Specify the exact origin
+    methods: ['GET', 'POST', 'PUT', 'OPTIONS', 'HEAD'], // Allow these methods
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'], // Specify headers
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
 
-
-app.options('*', cors()); // Handle preflight requests
+app.use(cors(corsOptions)); // Apply CORS middleware
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use('/general', generalRoute)
 
-const server = http.createServer(app);
+const server = https.createServer(app);
 
 websocket(server);
 
 server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
